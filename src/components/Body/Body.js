6369a373806554fd0963d616288e72bd5326af8f
Brian chow <brianchowshunfu@gmail.com>
2022-03-10 12:26:41 +0800

    spring

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -7,6 +7,8 @@ import { IconContext } from "react-icons";
 import { BsStarFill } from 'react-icons/bs'; */
 import { MdExpandMore } from 'react-icons/md';
 
+import { useSpring, animated } from 'react-spring'
+
 
 export default function Body({
     CardData, filterTaskByType, setData, submittingStatue, setStatus, setStar, filterTaskByDate, setFilterTaskByDate, setFilterTaskByType,
@@ -26,6 +28,7 @@ export default function Body({
 
     const handleStart = () => {
         setAdd(false)
+        toggle(false)
     }
  
     useEffect(() => {
@@ -92,8 +95,12 @@ export default function Body({
     } 
 
     //
-
-    
+    const [state, toggle] = useState(true)
+    const { x } = useSpring({
+        from: { x: 0 },
+        x: state ? 1 : 0,
+        config: { duration: 1000 },
+      })
 
     return( 
         <div className="main-body">
@@ -132,7 +139,20 @@ export default function Body({
                 {isFilter &&
                     <div className="opening-task">
                         <p>Are you ready to use Task Manager?</p>
-                        <button className="start-button" onClick={handleStart}  ><span>START</span></button>
+                        {/* <button className="start-button" onClick={handleStart}  ><span>START</span></button> */}
+                        <button className="start-button" onClick={handleStart}  >
+                            <animated.div
+                                className="spring"
+                                style={{
+                                opacity: x.to({ range: [0, 1], output: [0.3, 1] }),
+                                scale: x.to({
+                                    range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+                                    output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1],
+                                }),
+                                }}>
+                                <span>START</span>
+                            </animated.div>
+                        </button>
                     </div>
                 }
                 {filterTaskByType.map((task) => {
